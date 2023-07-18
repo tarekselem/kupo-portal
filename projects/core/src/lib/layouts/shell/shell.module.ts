092a483c86399a3d3e283947f8993902140b1895
Tarek Selem <t.hamdy91@yahoo.com>
2023-07-18 13:10:15 +0200

    installed angular-material

--- projects/core/src/lib/layouts/shell/shell.module.ts
+++ projects/core/src/lib/layouts/shell/shell.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+
 import { ShellComponent } from './shell.component';
 
 @NgModule({
-  imports: [],
+  imports: [
+    FlexLayoutModule,
+    FontAwesomeModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule,
+  ],
   providers: [],
   declarations: [ShellComponent],
   exports: [ShellComponent],
--- projects/kupo-ui/src/app/app.module.ts
+++ projects/kupo-ui/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { ShellModule } from 'projects/core/src/public-api';
 import { ShellModule } from '@core/layouts';
 
 @NgModule({
