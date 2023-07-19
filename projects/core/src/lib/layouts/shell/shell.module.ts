17bb23f9214c0b81c6153e2127266a5a084eecc9
Tarek Selem <t.hamdy91@yahoo.com>
2023-07-19 10:58:57 +0200

    added cards

--- projects/core/src/lib/layouts/shell/shell.module.ts
+++ projects/core/src/lib/layouts/shell/shell.module.ts
@@ -4,17 +4,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
 import { ShellComponent } from './shell.component';
 
 @NgModule({
   imports: [
     FlexLayoutModule,
-    FontAwesomeModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatDividerModule,
+    MatCardModule,
   ],
   providers: [],
   declarations: [ShellComponent],
